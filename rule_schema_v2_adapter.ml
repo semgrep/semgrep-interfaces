b9925be995e116627c9485dbbd41cadc2ebf3f34
pad <pad@r2c.dev>
2023-11-06 17:02:26 +0100

    comment

--- rule_schema_v2_adapter.ml
+++ rule_schema_v2_adapter.ml
@@ -1,3 +1,7 @@
+(* ATD adapter for rule_schema_v2.atd
+ * See https://atd.readthedocs.io/en/latest/atdgen.html#field-adapter-ocaml
+ *)
+
 module Formula = struct
 
   (** Convert from original json to ATD-compatible json *)
@@ -8,7 +12,7 @@ module Formula = struct
     | x -> x
 
   (** Convert from ATD-compatible json to original json *)
-  let restore  (atd : Yojson.Safe.t) : Yojson.Safe.t =
-    atd
-  
+  let restore  (_atd : Yojson.Safe.t) : Yojson.Safe.t =
+    (* not needed for now; we care just about parsing *)
+    failwith "Rule_schema_v2_adapter.Formula.restore not implemented"
 end
