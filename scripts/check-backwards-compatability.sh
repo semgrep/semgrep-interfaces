a0b47622ab6d900b91635f7c0cba82c067d5e694
Jonas Bergler <jonas@semgrep.com>
2023-11-08 08:05:48 +1300

    run lint in container (#172)
    Now that the required version of atddiff is available in our base ocaml
    container, we can use that for much faster runs.

--- scripts/check-backwards-compatability.sh
+++ scripts/check-backwards-compatability.sh
@@ -10,13 +10,12 @@
 #    - Diff against origin/main to establish a baseline
 #    - Diff against HEAD
 #    - Diff the two diffs to see if new issues were introduced
-
-set -u # no "-eo pipefail" because we do our own error handling
+set -euo pipefail
 
 minimum="v$(curl -s https://semgrep.dev/api/check-version | jq -r '.versions.minimum')"
 tags=$(git log --simplify-by-decoration --pretty=format:%D "${minimum}^!" origin/main | grep -o 'tag: [^,)]\+' | sed 's/^tag: //' | sort -n)
 
-checked=()
+checked=("dummy")
 errors=0
 for tag in $tags; do
     commit=$(git rev-list -n 1 "$tag")
@@ -26,17 +25,29 @@ for tag in $tags; do
     fi
     checked+=("$commit")
 
+    set +e # do our own error handling for a bit
     echo "Checking backward compatibility of semgrep_output_v1.atd against past version $tag"
-    git difftool -x 'atddiff --backward' -y "$tag" "origin/main" semgrep_output_v1.atd > before.txt
-    git difftool -x 'atddiff --backward' -y "$tag" "HEAD" semgrep_output_v1.atd > after.txt
+    git difftool --trust-exit-code -x 'atddiff --no-locations --backward' -y "$tag" "origin/main" semgrep_output_v1.atd > before.txt
+    ret=$?
+    if [ "$ret" -ge 1 ] && [ "$ret" -le 2 ]; then
+        echo "ERROR: atddiff had an error: $?"
+        cat before.txt
+        exit 1
+    fi
+    git difftool --trust-exit-code -x 'atddiff --no-locations --backward' -y "$tag" "HEAD" semgrep_output_v1.atd > after.txt
+    ret=$?
+    if [ "$ret" -ge 1 ] && [ "$ret" -le 2 ]; then
+        echo "ERROR: atddiff had an error: $?"
+        cat after.txt
+        exit 1
+    fi
 
-    # neccesary because filenames have temp paths and line numbers can change without causing issues
-    expr='s|File "/.*/\(.*.atd\)", line .*$|File "\1", line <removed for diff>|g'
-    diff -u <(sed "$expr" before.txt) <(sed "$expr" after.txt)
+    diff -u <(cat before.txt) <(cat after.txt)
     if [ "$?" -ne 0 ]; then
         echo "ERROR: semgrep_output_v1.atd is not backward compatible with $tag"
         errors=$((errors + 1))
     fi
+    set -e
 done
 
 if [ "$errors" -ne 0 ]; then
