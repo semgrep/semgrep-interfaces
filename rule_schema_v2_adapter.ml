b0eae2df924b6078065c93bdab3115390e00f858
Yoann Padioleau <pad@r2c.dev>
2023-11-08 21:34:38 +0100

    Improvement to rule_schema_v2.atd (#186)
    It can now parse many rules, even though the where
    part is unfinished
    
    test plan:
    see related PR in semgrep
    
    
    - [x] I ran `make setup && make` to update the generated code after
    editing a `.atd` file (TODO: have a CI check)
    - [x] I made sure we're still backward compatible with old versions of
    the CLI.
    For example, the Semgrep backend need to still be able to *consume* data
    generated
    	  by Semgrep 1.17.0.
    See
    https://atd.readthedocs.io/en/latest/atdgen-tutorial.html#smooth-protocol-upgrades

--- rule_schema_v2_adapter.ml
+++ rule_schema_v2_adapter.ml
@@ -8,7 +8,13 @@ module Formula = struct
   let normalize (orig : Yojson.Safe.t ) : Yojson.Safe.t =
     match orig with
     | `String str ->
-        `List [`String "pattern"; `String str]
+        `Assoc ["f", `List [`String "pattern"; `String str]]
+    | `Assoc [(key , elt)] ->
+        `Assoc ["f", `List [`String key; elt]]
+    | `Assoc [(key , elt); ("where", stuff)] ->
+        `Assoc [
+            ("f", `List [`String key; elt]);
+            ("where", stuff)]
     | x -> x
 
   (** Convert from ATD-compatible json to original json *)
@@ -17,14 +23,36 @@ module Formula = struct
     failwith "Rule_schema_v2_adapter.Formula.restore not implemented"
 end
 
-module Where = struct
+module Condition = struct
 
   (** Convert from original json to ATD-compatible json *)
-  let normalize (_orig : Yojson.Safe.t ) : Yojson.Safe.t =
-    failwith "TODO: Where.normalize"
+  let normalize (orig : Yojson.Safe.t ) : Yojson.Safe.t =
+    match orig with
+    | `Assoc (("comparison", cmp)::rest) ->
+       `List [`String "C";
+            `Assoc (("comparison", cmp)::rest)]
+    | `Assoc [("metavariable", mvar); ("regex", reg)] ->
+       `List [`String "M";
+            `Assoc [("metavariable", mvar); 
+              ("c", `List [`String "regex"; reg])]]
+    | `Assoc [("metavariable", mvar); ("type", ty)] ->
+       `List [`String "M";
+            `Assoc [("metavariable", mvar); 
+              ("c", `List [`String "type"; ty])]]
+    | `Assoc [("metavariable", mvar); ("types", tys)] ->
+       `List [`String "M";
+            `Assoc [("metavariable", mvar); 
+              ("c", `List [`String "types"; tys])]]
+    | `Assoc [("metavariable", mvar); ("pattern", p)] ->
+       `List [`String "M";
+            `Assoc [("metavariable", mvar); 
+              ("c", `List [`String "F"; `Assoc [("pattern", p)]])]]
+    | x -> 
+          x
+
 
   (** Convert from ATD-compatible json to original json *)
   let restore  (_atd : Yojson.Safe.t) : Yojson.Safe.t =
     (* not needed for now; we care just about parsing *)
-    failwith "Rule_schema_v2_adapter.Where.restore not implemented"
+    failwith "Rule_schema_v2_adapter.Condition.restore not implemented"
 end
