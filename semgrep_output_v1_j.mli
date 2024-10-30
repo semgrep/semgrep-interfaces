8669f341f931072fd4023be0e735f9b15a63940b
Ben Kettle <ben@semgrep.com>
2024-10-30 13:01:47 -0700

    deriving eq, yojson

--- semgrep_output_v1_j.mli
+++ semgrep_output_v1_j.mli
@@ -658,7 +658,8 @@ type partial_scan_result = Semgrep_output_v1_t.partial_scan_result
 
 type output_format = Semgrep_output_v1_t.output_format
 
-type manifest_kind = Semgrep_output_v1_t.manifest_kind [@@deriving show]
+type manifest_kind = Semgrep_output_v1_t.manifest_kind
+  [@@deriving show, eq, yojson]
 
 type manifest = Semgrep_output_v1_t.manifest = {
   kind: manifest_kind;
