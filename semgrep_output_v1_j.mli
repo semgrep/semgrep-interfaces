4ff22426dfa6e5607ac99f503f4f3a9181155a92
Nicholas Hakmiller <nicholas.hakmiller@semgrep.com>
2025-08-19 14:56:08 -0700

    chore(cli): revert unneeded new parameter

--- semgrep_output_v1_j.mli
+++ semgrep_output_v1_j.mli
@@ -746,7 +746,6 @@ type scan_metadata = Semgrep_output_v1_t.scan_metadata = {
   cli_version: version;
   unique_id: uuid;
   requested_products: product list;
-  compress_config: bool;
   dry_run: bool;
   sms_scan_id: string option
 }
