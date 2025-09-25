8baadf6b8604b59c9a660dbb371726c12a3666b8
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-09-25 23:32:25 +0530

    feat: add Gosu language with develop maturity (#408)
    - [x] I ran `make setup && make` to update the generated code after
    editing a `.atd` file (TODO: have a CI check)
    - [x] I made sure we're still backward compatible with old versions of
    the CLI.
    For example, the Semgrep backend need to still be able to *consume* data
    	  generated by Semgrep 1.50.0.
    See
    https://atd.readthedocs.io/en/latest/atdgen-tutorial.html#smooth-protocol-upgrades
    	  Note that the types related to the semgrep-core JSON output or the
    	  semgrep-core RPC do not need to be backward compatible!
    - [x] Any accompanying changes in `semgrep-proprietary` are approved and
    ready to merge once this PR is merged

--- Language.ml
+++ Language.ml
@@ -14,6 +14,7 @@ type t =
 | Dockerfile
 | Elixir
 | Go
+| Gosu
 | Hack
 | Html
 | Java
@@ -239,6 +240,19 @@ let list = [
   reverse_exts = None;
   shebangs = [];
   tags = [];
+};
+{
+  id = Gosu;
+  id_string = "gosu";
+  name = "Gosu";
+  keys = [{|gosu|}];
+  exts = [{|.gs|}];
+  maturity = Develop;
+  example_ext = None;
+  excluded_exts = [];
+  reverse_exts = None;
+  shebangs = [];
+  tags = [{|is_proprietary|}];
 };
 {
   id = Hack;
--- Language.mli
+++ Language.mli
@@ -14,6 +14,7 @@ type t =
 | Dockerfile
 | Elixir
 | Go
+| Gosu
 | Hack
 | Html
 | Java
